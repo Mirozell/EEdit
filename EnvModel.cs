efc2f5dd462625fd5422f3bfb644e9ff6ffd6b14
Mirozell <mirozell@gmail.com>
2011-06-21 23:09:05 -0500

    Cleaned up unused using statements

--- EnvModel.cs
+++ EnvModel.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using Microsoft.Win32;
-using System.Collections;
-using System.IO;
 
 namespace EEdit
 {
--- EnvVariable.cs
+++ EnvVariable.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Linq;
 
 namespace EEdit
 {
