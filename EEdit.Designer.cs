f290ab26ceaeee8a478b9e884bf6d62f946e1d90
Mirozell <mirozell@gmail.com>
2011-06-21 23:35:35 -0500

    Licensed under GPLv3.

--- EEdit.Designer.cs
+++ EEdit.Designer.cs
@@ -30,19 +30,21 @@
         {
             this.EnvSelectionTabs = new System.Windows.Forms.TabControl();
             this.MachineTab = new System.Windows.Forms.TabPage();
-            this.UserTab = new System.Windows.Forms.TabPage();
-            this.AboutTab = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.WebsiteLink = new System.Windows.Forms.LinkLabel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.MachineEditor = new EEdit.EnvEditor();
+            this.UserTab = new System.Windows.Forms.TabPage();
             this.UserEditor = new EEdit.EnvEditor();
+            this.AboutTab = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.WebsiteLink = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LicenseLink = new System.Windows.Forms.LinkLabel();
             this.EnvSelectionTabs.SuspendLayout();
             this.MachineTab.SuspendLayout();
             this.UserTab.SuspendLayout();
@@ -74,6 +76,16 @@
             this.MachineTab.Text = "Machine";
             this.MachineTab.UseVisualStyleBackColor = true;
             // 
+            // MachineEditor
+            // 
+            this.MachineEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.MachineEditor.Location = new System.Drawing.Point(0, 0);
+            this.MachineEditor.Name = "MachineEditor";
+            this.MachineEditor.Size = new System.Drawing.Size(665, 392);
+            this.MachineEditor.TabIndex = 0;
+            // 
             // UserTab
             // 
             this.UserTab.Controls.Add(this.UserEditor);
@@ -85,8 +97,17 @@
             this.UserTab.Text = "User";
             this.UserTab.UseVisualStyleBackColor = true;
             // 
+            // UserEditor
+            // 
+            this.UserEditor.Location = new System.Drawing.Point(0, 0);
+            this.UserEditor.Name = "UserEditor";
+            this.UserEditor.Size = new System.Drawing.Size(665, 392);
+            this.UserEditor.TabIndex = 0;
+            // 
             // AboutTab
             // 
+            this.AboutTab.Controls.Add(this.LicenseLink);
+            this.AboutTab.Controls.Add(this.label9);
             this.AboutTab.Controls.Add(this.label8);
             this.AboutTab.Controls.Add(this.label7);
             this.AboutTab.Controls.Add(this.label6);
@@ -103,35 +124,55 @@
             this.AboutTab.Text = "About";
             this.AboutTab.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // label8
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 46);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
-            this.label1.TabIndex = 0;
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(286, 227);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "1.0";
             // 
-            // label2
+            // label7
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(231, 155);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Website:";
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(235, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Version:";
             // 
-            // linkLabel1
+            // label6
             // 
-            this.WebsiteLink.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.WebsiteLink.AutoSize = true;
-            this.WebsiteLink.Location = new System.Drawing.Point(286, 155);
-            this.WebsiteLink.Name = "linkLabel1";
-            this.WebsiteLink.Size = new System.Drawing.Size(165, 13);
-            this.WebsiteLink.TabIndex = 2;
-            this.WebsiteLink.TabStop = true;
-            this.WebsiteLink.Text = "https://github.com/Mirozell/EEdit";
-            this.WebsiteLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.WebsiteLink_LinkClicked);
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(286, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Mirozell";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(239, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Author:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "A Windows environment variable editor.";
             // 
             // label3
             // 
@@ -144,72 +185,57 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "EEdit";
             // 
-            // label4
+            // WebsiteLink
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(244, 87);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(194, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "A Windows environment variable editor.";
+            this.WebsiteLink.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.WebsiteLink.AutoSize = true;
+            this.WebsiteLink.Location = new System.Drawing.Point(286, 155);
+            this.WebsiteLink.Name = "WebsiteLink";
+            this.WebsiteLink.Size = new System.Drawing.Size(165, 13);
+            this.WebsiteLink.TabIndex = 2;
+            this.WebsiteLink.TabStop = true;
+            this.WebsiteLink.Text = "https://github.com/Mirozell/EEdit";
+            this.WebsiteLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.WebsiteLink_LinkClicked);
             // 
-            // label5
+            // label2
             // 
-            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(239, 192);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Author:";
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Website:";
             // 
-            // label6
+            // label1
             // 
-            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(286, 192);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(42, 13);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Mirozell";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
             // 
-            // label7
+            // label9
             // 
-            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(235, 229);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(45, 13);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "Version:";
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(233, 263);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "License:";
             // 
-            // label8
+            // LicenseLink
             // 
-            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(286, 229);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(22, 13);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "1.0";
-            // 
-            // MachineEditor
-            // 
-            this.MachineEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.MachineEditor.Location = new System.Drawing.Point(0, 0);
-            this.MachineEditor.Name = "MachineEditor";
-            this.MachineEditor.Size = new System.Drawing.Size(665, 392);
-            this.MachineEditor.TabIndex = 0;
-            // 
-            // UserEditor
-            // 
-            this.UserEditor.Location = new System.Drawing.Point(0, 0);
-            this.UserEditor.Name = "UserEditor";
-            this.UserEditor.Size = new System.Drawing.Size(665, 392);
-            this.UserEditor.TabIndex = 0;
+            this.LicenseLink.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.LicenseLink.AutoSize = true;
+            this.LicenseLink.Location = new System.Drawing.Point(286, 263);
+            this.LicenseLink.Name = "LicenseLink";
+            this.LicenseLink.Size = new System.Drawing.Size(40, 13);
+            this.LicenseLink.TabIndex = 10;
+            this.LicenseLink.TabStop = true;
+            this.LicenseLink.Text = "GPLv3";
+            this.LicenseLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LicenseLink_LinkClicked);
             // 
             // EEditForm
             // 
@@ -247,6 +273,8 @@
         private System.Windows.Forms.LinkLabel WebsiteLink;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel LicenseLink;
+        private System.Windows.Forms.Label label9;
 
 
 
--- EEdit.cs
+++ EEdit.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+EEdit - A Windows Environment Editor
+Copyright (C) 2011  Mirozell
+
+Website: https://github.com/Mirozell/EEdit
+
+EEdit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EEdit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with EEdit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -21,6 +41,12 @@ namespace EEdit
         {
             WebsiteLink.LinkVisited = true;
             Process.Start(WebsiteLink.Text);
+        }
+
+        private void LicenseLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LicenseLink.LinkVisited = true;
+            Process.Start("http://www.gnu.org/licenses/gpl.txt");
         }
     }
 }
