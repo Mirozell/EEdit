6dbf51f21ecb053efc0cb6589c328ed7e90be949
Mirozell <mirozell@gmail.com>
2011-06-23 00:38:35 -0500

    Separated the GUI and the model into different projects.

--- Environment/EnvVariable.cs
+++ Environment/EnvVariable.cs
@@ -24,23 +24,21 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
 
-namespace EEdit
+namespace EEdit.Environment
 {
-    class EnvVariable
+    public class EnvVariable
     {
         public string Variable { get; private set; }
-
         public bool Deleted { get; private set; }
         public bool Added { get; private set; }
         public bool Edited { get; private set; }
 
-        private readonly ObservableCollection<string> Entries;
-
         public event NotifyCollectionChangedEventHandler CollectionChanged;
 
-        private string splitChar = ";";
+        private readonly ObservableCollection<string> Entries;
+        private const string splitChar = ";";
 
-        public EnvVariable(string variable, string value, bool setAdded)
+        internal EnvVariable(string variable, string value, bool setAdded)
         {
             if (string.IsNullOrWhiteSpace(variable)) throw new ArgumentException("Variable cannot be null, empty, or whitespace", "Variable");
 
@@ -71,46 +69,7 @@ namespace EEdit
             }
         }
 
-        private string SetEntries(string value)
-        {
-            value = value ?? "";
-            Entries.Clear();
-
-            string[] entries = value.Split(
-                new string[] { splitChar },
-                StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string entry in entries)
-            {
-                Entries.Add(entry);
-            }
-
-            return value;
-        }
-
-        public void ClearChangeStates()
-        {
-            Edited = false;
-            Added = false;
-            Deleted = false;
-        }
-
-        public void ClearDeletedState()
-        {
-            Deleted = false;
-        }
-
-        public void Delete()
-        {
-            Deleted = true;
-        }
-
         #region Entries Access Wrappers
-
-        private void Entries_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            CollectionChanged(sender, e);
-        }
 
         public void MoveEntry(int oldIndex, int newIndex)
         {
@@ -147,5 +106,44 @@ namespace EEdit
         }
 
         #endregion
+
+        internal void ClearChangeStates()
+        {
+            Edited = false;
+            Added = false;
+            Deleted = false;
+        }
+
+        internal void ClearDeletedState()
+        {
+            Deleted = false;
+        }
+
+        internal void Delete()
+        {
+            Deleted = true;
+        }
+
+        private string SetEntries(string value)
+        {
+            value = value ?? "";
+            Entries.Clear();
+
+            string[] entries = value.Split(
+                new string[] { splitChar },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string entry in entries)
+            {
+                Entries.Add(entry);
+            }
+
+            return value;
+        }
+
+        private void Entries_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanged(sender, e);
+        }
     }
 }
